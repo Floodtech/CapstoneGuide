42edc5eccd6a0bf600469b13f920520a2c960127
Ramon Mc <81272791+ramonmc876@users.noreply.github.com>
2023-06-06 21:53:07 -0500

    Update auth.py

--- App/views/auth.py
+++ App/views/auth.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
+from flask import Blueprint, render_template, jsonify, request
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from flask_login import login_required, login_user, current_user, logout_user
 
-from.index import index_views
+from .index import index_views
 
 from App.controllers import (
     create_user,
     jwt_authenticate,
-    login 
+    login
 )
 
 auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
@@ -16,17 +16,15 @@ auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
 Page/Action Routes
 '''
 
-"""@auth_views.route('/users', methods=['GET'])
+@auth_views.route('/users', methods=['GET'])
 def get_user_page():
     users = get_all_users()
     return render_template('users.html', users=users)
-
 
 @auth_views.route('/identify', methods=['GET'])
 @login_required
 def identify_page():
     return jsonify({'message': f"username: {current_user.username}, id : {current_user.id}"})
-
 
 @auth_views.route('/login', methods=['POST'])
 def login_action():
@@ -60,13 +58,13 @@ def create_user_endpoint():
 
 @auth_views.route('/api/login', methods=['POST'])
 def user_login_api():
-  data = request.json
-  token = jwt_authenticate(data['username'], data['password'])
-  if not token:
-    return jsonify(message='bad username or password given'), 401
-  return jsonify(access_token=token)
+    data = request.json
+    token = jwt_authenticate(data['username'], data['password'])
+    if not token:
+        return jsonify(message='bad username or password given'), 401
+    return jsonify(access_token=token)
 
 @auth_views.route('/api/identify', methods=['GET'])
 @jwt_required()
 def identify_user_action():
-    return jsonify({'message': f"username: {jwt_current_user.username}, id : {jwt_current_user.id}"})"""
+    return jsonify({'message': f"username: {jwt_current_user.username}, id : {jwt_current_user.id}"})
